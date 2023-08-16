01cf667065fe24a5ec07f5cbd6d505b15767b4bd
amer121 <109520654+amiroumous121@users.noreply.github.com>
2023-08-16 19:31:53 +1000

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,23 @@
+console.log("JavaScript is linked!");
+let str = "Hello, World!";
+
+console.log(str.length); // Outputs the length of the string
+console.log(str.split(",")); // Splits the string into an array at each comma
+console.log(str.replace("Hello", "Hi")); // Replaces "Hello" with "Hi" in the string
+console.log(str.toUpperCase()); // Converts the string to uppercase
+console.log(str.toLowerCase()); // Converts the string to lowercase
+let numArray = [1, 2, 3, 4, 5];
+let strArray = ["Hello", "World", "!"];
+
+console.log(numArray.length); // Outputs the length of the number array
+console.log(numArray.sort()); // Sorts the number array
+console.log(strArray.join(" ")); // Joins the string array into a single string with spaces between each item
+console.log(strArray.pop()); // Removes the last item of the string array and outputs it
+console.log(strArray.push("Hello again")); // Adds "Hello again" to the end of the string array and outputs the new length
+let date = new Date();
+
+console.log(date.getFullYear()); // Outputs the current year
+console.log(date.getMonth()); // Outputs the current month
+console.log(date.getDate()); // Outputs the current day of the month
+console.log(date.getHours()); // Outputs the current hour
+console.log(date.getMinutes()); // Outputs the current minute
